5a8bb166bbcaefc281e148ec2c1730d72032b962
Sagat9881 <Sagat9881@gmail.com>
2020-09-14 18:09:49 +0500

    Message For

---
+++ src/main/java/org/example/Task3.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        int a = 21321230;
+        Task3.lastBitRevers(a);
+    }
+
+    static void lastBitRevers(int a) {
+
+        if (!(a % 10 == 0)) System.out.println("Введите корректное число");
+
+        int b = a + 1;
+
+        int c = b | a;
+        System.out.println(c);
+    }
+}
