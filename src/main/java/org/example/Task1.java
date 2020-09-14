775e24d70d49f774d96729476b9c91f9962c3a0e
Sagat9881 <Sagat9881@gmail.com>
2020-09-14 18:56:01 +0500

    Message For

--- src/main/java/org/example/Task1.java
+++ src/main/java/org/example/Task1.java
@@ -9,24 +9,24 @@ public class Task1 {
             Integer[] mass = {3, 5, 23, 54, 123, 54, 23, 12, 43, 6, 324, 12, 34, 2, 44, 1, 3, 56, 7, 2,
                     54, 6, 423, 435, 213, 2, 4, 2, 6, 7, 8, 9, 73, 64, 8, 6, 90, 0,3};
 
-            Map<Integer, AtomicInteger> map= new HashMap<>();
+            Map<Integer, AtomicInteger> map= new HashMap<>();  // иницируем создание буферного списка, в котором будем сохранять число и количество вхождений
 
-            for (int i = 0; i < mass.length; i++) {
+            for (int i = 0; i < mass.length; i++) {            // заполняем буфер значениями из исходного массива по типу: число-ключ, количество вхождений - значение
                 if(map.containsKey(mass[i])){
-                    map.get(mass[i]).incrementAndGet();
+                    map.get(mass[i]).incrementAndGet();        // Если буфер уже содержит число-ключ mass[i], то увеличиваем кол-во вхождения на 1
                 } else{
-                    map.put(mass[i], new AtomicInteger(1));
+                    map.put(mass[i], new AtomicInteger(1)); // Помещаем в буфер число-ключ(mass[i]), устанавливая количество вхождений на "1"
                 }
             }
-            List<Map.Entry<Integer, AtomicInteger>> entries = new ArrayList<>(map.entrySet());
-            Collections.sort(entries, new Comparator<Map.Entry<Integer, AtomicInteger>>() {
+            List<Map.Entry<Integer, AtomicInteger>> entries = new ArrayList<>(map.entrySet());  // Иницируем создание итогового списка
+            Collections.sort(entries, new Comparator<Map.Entry<Integer, AtomicInteger>> () {     // сортируем итоговый список с помощью инициализации анонимного компаратора, используя дефолтную реализацию класса Long
                 public int compare(Map.Entry<Integer, AtomicInteger> a, Map.Entry<Integer, AtomicInteger> b) {
                     return Long.compare(
                             a.getValue().get() * ((long) Integer.MAX_VALUE) + a.getKey(),
                             b.getValue().get() * ((long) Integer.MAX_VALUE) + b.getKey());
                 }
             });
-            for (Map.Entry<Integer, AtomicInteger> entry : entries) {
+            for (Map.Entry<Integer, AtomicInteger> entry : entries) {       // распечатка итогового списка
                 System.out.println(entry.getKey() + "\t" + entry.getValue());
             }
         }
--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -2,48 +2,47 @@ package org.example;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class Task2 {
 
     public static void main(String[] args) {
 
-         String expression = "12[3б,][б  ([)] ]{}()45";
+        String expression = "12[3б,][б  ([)] ]{}()45";
 
-         System.out.println((isBalanced(expression)) ? "YES" : "NO");
-        }
+        System.out.println((isBalanced(expression)) ? "YES" : "NO");
+    }
 
 
     public static boolean isBalanced(String expression) {
 
-        expression = expression.replaceAll("[!\"#$%&'*+,-./:;<=>?@_~\\s]","")
-                               .replaceAll("[0-9]","")
-                               .replaceAll("[а-яА-Я]","");
+        expression = expression.replaceAll("[!\"#$%&'*+,-./:;<=>?@_~\\s]", "")  // удаляем из строки все символы, кроме скобок
+                .replaceAll("[0-9]", "")
+                .replaceAll("[а-яА-Я]", "");
 
 
-        char[] braces = expression.toCharArray();
-        if (braces.length % 2 != 0) {
+        char[] braces = expression.toCharArray();                   // превращаем нормализованную строку в массив Char'ов
+        if (braces.length % 2 != 0) {                               // Если длинна массива из нормализованной строки нечетная, значит одной парной скобки не хватает
             return false;
         }
 
-        Map<Character, Character> endbrackets = new HashMap<>();
+        Map<Character, Character> endbrackets = new HashMap<>();   // буферный список, в котором мы связываем открывающую и закрывающую скобки для удобства проверки
         endbrackets.put('{', '}');
         endbrackets.put('[', ']');
         endbrackets.put('(', ')');
-        if (endbrackets.get(braces[0]) == null) {
+        if (endbrackets.get(braces[0]) == null) {                  // если первый элемент нормализованного массива не открывающая скобка, значит нарушена парность скобок и дальнейшие проверки не имеют смысла
             return false;
         }
-        Stack <Character> stack = new Stack<>();
-        for (Character c : braces) {
-            if (stack.isEmpty() && endbrackets.get(c) == null) {
-                return false;
-            } else if (stack.isEmpty() || endbrackets.get(c) != null) {
-                stack.push(c);
-            } else if (!stack.isEmpty() && endbrackets.get(stack.peek()).equals(c)) {
-                stack.pop();
-            } else {
-                return false;
+        Stack<Character> stack = new Stack<>();                                          // иницируем создание стека
+        for (Character c : braces) {                                                     // обходим каждый эелемент исходного нормализованного массива
+            if (stack.isEmpty() && endbrackets.get(c) == null) {                         // если стек пустой или значение из нормализованного массива, используемое в качестве ключа к буферному списку, возвращает null
+                return false;                                                            // то возвращаем false т.к. первый элемент нормализованного массива - открывающая скобка
+            } else if (stack.isEmpty() || endbrackets.get(c) != null) {                  // стек пустой, но значение массива, используемое в качестве ключа к буферному списку не возвращает null
+                stack.push(c);                                                           // значит, это открывающая скобка и мы кладем ее на вершину стека
+            } else if (!stack.isEmpty() && endbrackets.get(stack.peek()).equals(c)) {    // стек не пустой, ключ с вершины стека возвращает значение, которое является закрывающей скобко.
+                stack.pop();                                                             // которое сравнивается с элементом массива braces [c]
+            } else {                                                                     // если возвращается true, значит braces [c] - закрывающая скобка, парность сохраняется
+                return false;                                                            // если возвращается false, значит braces [c] - другая закрывающая скобка. Парность нарушена
             }
         }
         return stack.size() == 0;
